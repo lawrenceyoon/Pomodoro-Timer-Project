c6349604eb1897a3940ce4720ce31366f9ea84c2
Lawrence Yoon <yoonlawrence@yahoo.com>
2021-05-23 16:43:43 -0700

    github images for readme

---
+++ build/precache-manifest.9f6cb3cf3635f505d8f8ec2bd327533c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a8f583805403a281e1158a0746f391",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bc7d8d2bb1142c1b784",
+    "url": "/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bc7d8d2bb1142c1b784",
+    "url": "/static/js/main.21e67ee9.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
